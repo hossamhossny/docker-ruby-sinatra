82745e8f4d31b778c008f6d52f5b86b4b1dc8812
Hossam Hossny <hossamhossnyar@gmail.com>
2018-03-15 22:39:43 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/jellybob/sinatra-example.git .
+git clone https://github.com/stefanobernardi/stripe-sinatra-example.git .
 gem install sinatra shotgun
 if [ "$RACK_ENV" == "production" ]; 
 then 
